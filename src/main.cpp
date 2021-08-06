35106c6b953beb2ec3641ce9b0245a5617ba3d4c
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2021-08-06 10:07:32 +0200

    writing to a string buffer (char array)

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,19 @@ void setup() {
 }
 
 /**
- * Open the serial monitor to see the output
+ * Space for strings up to 49 bytes (the last byte **must** be zero)
+ * Expect unexcpected behavior if writing strings longer than this,
+ * like system crashes, arbitrary variable changes etc...
  */
+char buffer[50];
+int counter = 0;
 void loop() {
-  Serial.println("Hello world!");
+  counter++;
+  sprintf(buffer, "Hallo zum %d-ten mal\n", counter);
+  Serial.print(buffer);
+  for (int p=16; p<24; p++) {
+    Serial.printf("b[%d]=%d='%c', ", p, buffer[p], buffer[p]);
+  }
+  Serial.println();  // new line
   delay(1000);
 }
